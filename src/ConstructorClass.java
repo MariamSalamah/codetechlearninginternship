19ec96e52fe79bb4740a7e52090b5b9046d61931
Mariam <mariam.mohamed581@gmail.com>
2024-11-13 12:35:23 +0200

    init commit

--- src/ConstructorClass.java
+++ src/ConstructorClass.java
@@ -1,13 +1,24 @@
 //package codetechinternship;
-public class ConstructorClass {
+//module 17
+//Write a code on Constructor in Java
+//Write a code on Parametrized Constructor in Java
+
+public class ConstructorClass{
     int x ;
     ConstructorClass(){
         x = 10;
     }
+    ConstructorClass(int i){
+        x = i;
+    }
 
-public static void main(String [] args){
-    ConstructorClass c1 = new ConstructorClass();
-    ConstructorClass c2 = new ConstructorClass();
-    System.out.println(c1.x +" " + c2.x);
-}
+    public static void main(String [] args){
+        ConstructorClass c1 = new ConstructorClass();
+        ConstructorClass c2 = new ConstructorClass();
+        System.out.println(c1.x +" " + c2.x);
+
+        ConstructorClass c3 = new ConstructorClass(20);
+        ConstructorClass c4 = new ConstructorClass(30);
+        System.out.println(c3.x +" " + c4.x);
+    }
 }
