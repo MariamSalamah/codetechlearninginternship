93518669f33658cc935f724870701c9524e18d37
Mariam <mariam.mohamed581@gmail.com>
2024-11-18 10:00:28 +0200

    try to init commit using terminal

--- src/DateDemo.java
+++ src/DateDemo.java
@@ -1,2 +1,14 @@
-package PACKAGE_NAME;public class DateDemo {
+//module 21
+//Date Class simple Demo
+import java.util.Date;
+public class DateDemo {
+    public static void main(String [] args){
+        Date date = new Date();
+        System.out.println(date.toString());
+//        Date makeADate = ;
+        System.out.println(date);
+
+    }
+
+
 }
