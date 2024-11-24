38628b4e596742e7e1e972e8d0289592db446f65
Mariam <mariam.mohamed581@gmail.com>
2024-11-24 10:59:56 +0200

    comment

--- src/SimpleDateFormatDemo.java
+++ src/SimpleDateFormatDemo.java
@@ -6,18 +6,26 @@ import java.util.Date;
 
 public class SimpleDateFormatDemo {
     public static void main(String [] args){
+        //TODO Auto generated method stub
+
         Date now = new Date();
         System.out.println(now);
 
         SimpleDateFormat dateNow = new SimpleDateFormat("E yyyy.MM.dd 'at' hh:mm:ss a zzz");
         System.out.println(dateNow.format(now));
 
+        // display time and date
+
         String str = String.format("Current Date/Time : %tc" , now);
         System.out.println(str);
         System.out.println();
 
+        // display time and date using $ sign
+
         System.out.printf("%1$s %2$tB %2$td, %2$tY", "Due date:",now);
         System.out.println();
+
+        // display time and date using < sign
 
         System.out.printf("%s %tB %<te, %<tY","Due date:",now);
 
